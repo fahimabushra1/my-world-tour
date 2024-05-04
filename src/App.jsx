de8304c212a87acbbfe19fdb5664b054b7b87726
Fahima Bushra <bushra.arifeen@gmail.com>
2024-05-04 01:14:22 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>MY <span style={{color:'red'}}>WORLD</span> MY TOUR</h1>
+        <h1 style={{color:'blue'}}>With React</h1>
+        <p>coding die jibon gori</p>
+      </div>
+    </>
+  )
+}
+
+export default App
